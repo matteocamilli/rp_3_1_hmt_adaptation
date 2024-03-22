682886ef24c49439d39734ca5b28367e2180cd26
Diego41ITA <diego.quattrone@mail.polimi.it>
2024-03-22 18:54:38 +0100

    calcolo di FTG e SCS per le configurazioni generate randomicamente

--- dataset_generator.py
+++ dataset_generator.py
@@ -28,12 +28,35 @@ constant_parameters = [
     "HUM_2_POS_Y",
     "ROB_1_CHG",
 ]
+result_df_columns = [
+    "PRGS",
+    "ORCH_1_Dstop",
+    "ORCH_1_Drestart",
+    "ORCH_1_Fstop",
+    "ORCH_1_Frestart",
+    "PSCS__TAU",
+    "HUM_1_VEL",
+    "HUM_2_VEL",
+    "HUM_1_FW",
+    "HUM_1_AGE",
+    "HUM_1_STA",
+    "HUM_2_FW",
+    "HUM_2_AGE",
+    "HUM_2_STA",
+    "HUM_1_POS_X",
+    "HUM_1_POS_Y",
+    "HUM_2_POS_X",
+    "HUM_2_POS_Y",
+    "ROB_1_VEL",
+    "ROB_1_CHG",
+]
+
 
 df = pd.read_csv("{}dataset{}.csv".format(DIR, POINTS))
 df = df.loc[(df['FTG_HUM_1'] >= 0.2) & (df['SCS'] < 1)]
 
 variables_domain = [(5.0, 7.5), (2.0, 4.5), (0.5, 0.8), (0.1, 0.4), (250.0, 700.0), (30.0, 100.0), (30.0, 100.0), (30.0, 100.0)]
-result_df = pd.DataFrame(columns=df.columns)
+result_df = pd.DataFrame(columns=result_df_columns)
 
 for idx, (_, row) in enumerate(df.iterrows()):
     random_generated_variables = []
@@ -41,7 +64,7 @@ for idx, (_, row) in enumerate(df.iterrows()):
         random_generated_variables.append(random.uniform(start, end))
     random_generated_variables = np.array(random_generated_variables).reshape((1, len(feature_names)))
 
-    result_local = pd.DataFrame(columns=df.columns)
+    result_local = pd.DataFrame(columns=result_df.columns)
     result_local[feature_names] = random_generated_variables
     result_local[constant_parameters] = df[constant_parameters].to_numpy()[idx]
     result_df = pd.concat([result_df, result_local], ignore_index=True)
--- results_validation.py
+++ results_validation.py
@@ -5,3 +5,31 @@ import matplotlib.pyplot as plt
 import os
 from joblib import dump, load
 
+regressor_SCS_path = "./regressors/regressor_SCS.joblib"
+regressor_FTG_path = "./regressors/regressor_FTG.joblib"
+df_path = "additional_datasets/randomly_generated_configuration.csv"
+df2_path = "initial_configurations_improved.csv"
+features = ["SCS", "FTG"]
+
+def calculateRegression(df): 
+    regressor_SCS = load(regressor_SCS_path)
+    regressor_FTG = load(regressor_FTG_path)
+    random_success_probability = regressor_SCS.predict(df) 
+    random_muscle_fatigue      = regressor_FTG.predict(df)
+    return (random_success_probability, random_muscle_fatigue)
+
+if __name__ == "__main__":
+    df = pd.read_csv(df_path)
+    result_df = pd.DataFrame(columns = features)
+    
+    for idx, row in df.iterrows(): 
+        regressors_input = pd.DataFrame([row])
+        random_values    = calculateRegression(regressors_input)
+        result_local     = pd.DataFrame(columns=result_df.columns)
+        result_local[features] = [random_values]
+        result_df              = pd.concat([result_df, result_local], ignore_index=True)
+
+    df2 = pd.read_csv(df2_path)
+    
+    NSGAII_SCS_FTG_values = df2[features]
+    random_SCS_FTG_values = result_df
